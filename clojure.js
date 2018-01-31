a7a1c8aaa9f76e28c51a3b245016ffd1b4e8008f
Raymond W. Ko <raymond.w.ko@gmail.com>
2018-01-31 16:58:01 -0500

    initial commit of clojure.esl

---
+++ clojure.js
@@ -0,0 +1,65 @@
+var slice = Array.prototype.slice;
+var m = module.exports;
+m['def'] = function () {
+    var name = arguments[0];
+    var value = arguments[1];
+    return {
+        type: 'list',
+        values: Array.prototype.concat([{
+                type: 'atom',
+                value: 'var'
+            }], [name], [value])
+    };
+};
+m['defn'] = function () {
+    var name = arguments[0];
+    var params = arguments[1];
+    var body = this.list.apply(null, slice.call(arguments, 2));
+    return {
+        type: 'list',
+        values: Array.prototype.concat([{
+                type: 'atom',
+                value: '='
+            }], [name], [{
+                type: 'list',
+                values: Array.prototype.concat([{
+                        type: 'atom',
+                        value: 'lambda'
+                    }], [params], body.values)
+            }])
+    };
+};
+m['defarray'] = function () {
+    var name = arguments[0];
+    var body = this.list.apply(null, slice.call(arguments, 1));
+    return {
+        type: 'list',
+        values: Array.prototype.concat([{
+                type: 'atom',
+                value: 'var'
+            }], [name], [{
+                type: 'list',
+                values: Array.prototype.concat([{
+                        type: 'atom',
+                        value: 'array'
+                    }], body.values)
+            }])
+    };
+};
+m['defobject'] = function () {
+    var name = arguments[0];
+    var body = this.list.apply(null, slice.call(arguments, 1));
+    return {
+        type: 'list',
+        values: Array.prototype.concat([{
+                type: 'atom',
+                value: 'var'
+            }], [name], [{
+                type: 'list',
+                values: Array.prototype.concat([{
+                        type: 'atom',
+                        value: 'object'
+                    }], body.values)
+            }])
+    };
+};
